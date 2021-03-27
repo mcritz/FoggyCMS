3a538ae6ea990cf971bb892e1f7dbeacf05b31e9
Michael Critz <github@michaelcritz.com>
2021-03-27 11:14:20 -0700

    Added routes for Assets

--- Sources/App/Controllers/AssetController.swift
+++ Sources/App/Controllers/AssetController.swift
@@ -7,6 +7,10 @@ final class AssetController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let assetGroup = routes.grouped("assets")
         assetGroup.get(use: index)
+        assetGroup.post(use: create)
+        assetGroup.group(":assetID") { asset in
+            asset.get(use: getOne)
+        }
     }
     
     func index(req: Request) throws -> EventLoopFuture<[Asset]> {
@@ -18,5 +22,16 @@ final class AssetController: RouteCollection {
             .decode(Asset.self)
             .save(on: req.db)
             .transform(to: Asset())
+    }
+    
+    func getOne(req: Request) throws -> EventLoopFuture<Response> {
+        return Asset.find(req.parameters.get("assetID"),
+                       on: req.db)
+            .flatMapThrowing { maybeAsset in
+                guard let asset = maybeAsset else {
+                    throw Abort(.notFound)
+                }
+                return req.fileio.streamFile(at: asset.filepath)
+            }
     }
 }
